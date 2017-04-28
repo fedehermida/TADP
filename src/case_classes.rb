535adc660f905647360e0b23ebed0b3902128265
fedehermida <fede.hermida@hotmail.com>
2017-04-28 04:53:21 -0300

    Finalizado el constructor con interpretación de errores.

--- src/case_classes.rb
+++ src/case_classes.rb
@@ -1,3 +1,4 @@
+
 class Symbol
   def < arg
     if arg.is_a? Class
@@ -21,38 +22,55 @@ module Inmutabilidad
       clazz = Class.new
     end
     Object.const_set name, clazz
-    clazz.class.send(:define_method, :<,Proc.new{ |m| puts "esto hereda de"} )
-    clazz.class_eval &block
+
+
 
     clazz.class_eval do
 
-
       def initialize(*args)
-        self.class.instance_variables.each_with_index do |valor, indice|
-          instance_variable_set("#{valor.to_s}",args.flatten.at(indice))
+        if(args.size == self.class.instance_variables.size)
+          self.class.instance_variables.each_with_index do |valor, indice|
+            instance_variable_set("#{valor.to_s}",args.flatten.at(indice))
+          end
+        else
+          raise "wrong number of arguments (given #{args.size.to_s}, expected #{self.class.instance_variables.size})"
         end
       end
 
-      def self.inherited(subclass)
-        raise "No se puede heredar de una Case_Class"
+      def method_missing(symbol,*args)
+
+        raise "Error! El metodo #{symbol.to_s} no esta definido!"
+
+
       end
 
 
+      def self.inherited(subclass)
+        raise "Herencia no permitida"
+      end
+    end
+
+    clazz.define_singleton_method :attr_accessor do
+    |*args|
+
+      args.each do |argumento|
+        define_method argumento do
+          instance_variable_get "@#{argumento.to_s}"
+        end
+
+        clazz.send(:instance_variable_set, "@#{argumento.to_s}".to_sym, 0)
+      end
     end
 
 
 
+    clazz.class_eval &block
+    clazz.freeze
 
     define_singleton_method clazz.to_s.to_sym do
     |*args|
-      if(args.size == clazz.instance_variables.size)
-
-        instancia = clazz.new(args)
-        instancia
-
-      else
-        raise "Numero de argumentos erroneos (se dieron #{args.size.to_s}, se esperaban #{clazz.instance_variables.size})"
-      end
+      instancia = clazz.new(*args)
+      instancia.freeze
     end
   end
 
@@ -85,41 +103,3 @@ end
 
 include Inmutabilidad
 
-class Menem
-  def hola
-    puts "menem"
-  end
-
-end
-
-case_class Guerrero  do
-  attr_reader :ataque, :defensa
-  @ataque = 0
-  @defensa = 0
-  #INICIALIZACION DE VARIABLES OBLIGATORIA!!!!!!!
-end
-
-
-
-
-
-case_object Scarcella < Menem do
-
-  def volar
-    puts "Andar en ojotas con 10 grados"
-  end
-
-  def dar_clase
-    puts "Me encantan los bloques"
-  end
-
-end
-
-unGuerrero = Guerrero(40,50)#LOS PARAMETROS SE PASAN EN EL ORDEN QUE FUERON DECLARADOS EN ATTR_READER
-puts (unGuerrero.ataque)
-puts (unGuerrero.defensa)
-
-scarcella = Scarcella
-scarcella.volar
-scarcella.dar_clase
-puts Guerrero.class
