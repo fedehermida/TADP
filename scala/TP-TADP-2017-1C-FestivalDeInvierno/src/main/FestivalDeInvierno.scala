f1c3e4803ae5fc5b6c3b9a07dbf9b46f2e9ab92d
TorricoJM <jmanueltorrico_0593@hotmail.com>
2017-06-09 13:38:21 -0300

    Add files via upload

--- scala/TP-TADP-2017-1C-FestivalDeInvierno/src/main/FestivalDeInvierno.scala
+++ scala/TP-TADP-2017-1C-FestivalDeInvierno/src/main/FestivalDeInvierno.scala
@@ -1,37 +1,48 @@
 package main
 
-/**
-  * Created by leonardo on 03/06/17.
-  */
+
 package object FestivalDeInvierno {
+  
+  trait Competidor {
+    def cargarPescados(cantidad:Int){
+      require(0 to this.cantMaxPescados() contains cantidad, "Cantidad no valida")
+    }
+    
+    def cantMaxPescados():Int = ???
+    def danio:Int = ???
+    
+  }
 
-  class Jinete(unVikingo:Vikingo,unDragon:Dragon){
-
+  case class Jinete(unVikingo:Vikingo,unDragon:Dragon) extends Competidor{
+    override def cantMaxPescados():Int = unDragon.peso / 5 - unVikingo.peso
+    
+    override def danio=unVikingo.danio + unDragon.danio
+    
+    def velocidad=unDragon.velocidad()-unVikingo.peso
   }
 
 
   case class Vikingo(item:Item,
-                    caracteristicas: Caracteristicas){
+                     caracteristicas: Caracteristicas) extends Competidor{
 
     def peso = this.caracteristicas.peso
     def velocidad=this.caracteristicas.velocidad
     def hambre=this.caracteristicas.hambre
     def barbarosidad=this.caracteristicas.barbarosidad
+    def tieneItem(unItem:Item):Boolean = item == unItem
+    override def danio=this.barbarosidad + item.danio
 
-    def calcularDanio():Int=5
-
-
-
-    def puedoMontarUnaRaza(unDragon:Dragon):Boolean={
-        unDragon.asInstanceOf match{
-          case _:NadderMortifero => unDragon.danio > this.calcularDanio()
-          case _:Gronckle=>Gronckle.pesoMaximo > this.peso
-
-        }
-    }
-
-
-  }
+    
+    override def cantMaxPescados():Int = peso / 2 + barbarosidad * 2
+    
+    def montar(unDragon:Dragon): Competidor =
+      if (this.puedeMontar(unDragon))
+        Jinete(this,unDragon)
+      else
+        this
+      
+    def puedeMontar(unDragon:Dragon): Boolean =
+      unDragon.requisitos.foldRight(true)((a,b) => a.cumpleRequisito(this, unDragon).&&(b))
   }
 
   case class Caracteristicas(peso:Int,
@@ -45,21 +56,77 @@ package object FestivalDeInvierno {
 
   }
 
-  abstract class Dragon(){
+  trait Atributos{
+    def velocidadBase: Int
+    def peso: Int
+    def danio: Int
+  }
+  
+  abstract class Dragon(
+      val requisitos: List[Requisito] = List(RequisitoBasico)) extends Atributos{
+    
+    def velocidadBase: Int = 60
+    def velocidad(): Int = velocidadBase - peso
 
-
-
+  }
+  
+  case class FuriaNocturna(
+      peso: Int,
+      danio: Int,
+      override val requisitos:List[Requisito]) extends Dragon(requisitos:List[Requisito]){
+    
+    override def velocidad(): Int = {
+      super.velocidad()*3
+    }
+    
+    
   }
 
 
-  case class NadderMortifero(velocidadBase:Int,peso:Int, danio:Int) extends Dragon{
-
-
-  }
-
-  case class Gronckle(velocidadBase:Int,peso:Int,danio:Int,pesoMaximo:Int) extends Dragon{
-
-
+  case class NadderMortifero(
+      peso: Int,
+      danio: Int = 150,
+      override val requisitos:List[Requisito] = List(RequisitoBasico,RequisitoDeDanio)) extends Dragon{
 
   }
 
+  case class Gronckle(
+      peso: Int,
+      danio: Int,
+      override val requisitos:List[Requisito]) extends Dragon(requisitos:List[Requisito]){
+    
+    override def velocidadBase = super.velocidadBase / 2
+   
+
+  }
+  
+  trait Requisito {
+    def cumpleRequisito(unVikingo:Vikingo, unDragon:Dragon): Boolean
+  }
+  
+  case object RequisitoBasico extends Requisito{
+    override def cumpleRequisito(unVikingo:Vikingo, unDragon:Dragon):Boolean =
+      unVikingo.peso <= unDragon.peso / 5
+  }
+  
+  case class RequisitoBarbarosidad(minimo:Int) extends Requisito{
+    def cumpleRequisito(unVikingo:Vikingo, unDragon:Dragon):Boolean =
+      unVikingo.barbarosidad > this.minimo
+  }
+  
+  case class RequisitoDeItem(unItem:Item) extends Requisito{
+    def cumpleRequisito(unVikingo:Vikingo, unDragon:Dragon):Boolean =
+      unVikingo.tieneItem(unItem)
+  }
+  
+  case object RequisitoDeDanio extends Requisito{
+    def cumpleRequisito(unVikingo:Vikingo, unDragon:Dragon):Boolean =
+      unVikingo.danio < unDragon.danio
+  }
+  
+  case class RequisitoDePeso(pesoMaximo: Int) extends Requisito{
+    def cumpleRequisito(unVikingo:Vikingo, unDragon:Dragon):Boolean =
+      unVikingo.peso <= pesoMaximo
+  }
+
+}
