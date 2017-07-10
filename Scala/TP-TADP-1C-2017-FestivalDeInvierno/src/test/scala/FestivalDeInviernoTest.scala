fe2884edcbded12f314f895b66b229e15bb2f452
leofs94 <leofs_94@hotmail.com>
2017-07-10 01:21:22 -0300

    test ultima version

--- Scala/TP-TADP-1C-2017-FestivalDeInvierno/src/test/scala/FestivalDeInviernoTest.scala
+++ Scala/TP-TADP-1C-2017-FestivalDeInvierno/src/test/scala/FestivalDeInviernoTest.scala
@@ -51,8 +51,8 @@ class FestivalDeInvierno extends FunSuite {
     var roco2 = FuriaNocturna(1,3,15,List(RequisitoBarbarosidad(8),RequisitoItem(SistemaDeVuelo)))
 
     var torneo=Torneo(List(roco,roco2),List(posta))
-
-    assertResult(torneo.jugar(List(vikingo,vikingo2),ReglasDeEliminacion(1)))(Some(Vikingo("Willy1",CaracteristicasVikingo(15,2,9,5),Some(SistemaDeVuelo),List())))
+    var estadoTorneo = RondaEnCurso(List(vikingo,vikingo2))
+    assertResult(torneo.jugar(estadoTorneo)(ReglasDeEliminacion(1)))(Some(Vikingo("Willy1",CaracteristicasVikingo(15,2,9,5),Some(SistemaDeVuelo),List())))
 
   }
 
@@ -67,7 +67,8 @@ class FestivalDeInvierno extends FunSuite {
     var roco = FuriaNocturna(1,3,15,List(RequisitoBarbarosidad(1)))
     var roco2 = FuriaNocturna(1,3,15,List(RequisitoBarbarosidad(1)))
     var torneo=Torneo(List(roco,roco2),List(posta,posta2,posta3))
-    assertResult(torneo.jugar(List(vikingo,vikingo2,vikingo3,vikingo4),ReglasDeEliminacion(1)))(Some(Vikingo("Willy3",CaracteristicasVikingo(13,2,7,15),None,List())))
+    var estadoTorneo = RondaEnCurso(List(vikingo,vikingo2,vikingo3,vikingo4))
+    assertResult(torneo.jugar(estadoTorneo)(ReglasDeEliminacion(1)))(Some(Vikingo("Willy3",CaracteristicasVikingo(13,2,7,15),None,List())))
 
 
   }
@@ -83,7 +84,7 @@ class FestivalDeInvierno extends FunSuite {
     var roco = FuriaNocturna(1,3,15,List(RequisitoBarbarosidad(1)))
     var roco2 = FuriaNocturna(1,3,15,List(RequisitoBarbarosidad(1)))
     var torneo=Torneo(List(roco,roco2),List(posta,posta3))
-    assertResult(torneo.jugar(List(vikingo,vikingo2,vikingo3,vikingo4), ReglasDeEliminacion(1)))(Some(Vikingo("Willy3",CaracteristicasVikingo(13,2,7,20),None,List())))
+    assertResult(torneo.jugar(RondaEnCurso(List(vikingo,vikingo2,vikingo3,vikingo4)))(ReglasDeEliminacion(1)))(Some(Vikingo("Willy3",CaracteristicasVikingo(13,2,7,20),None,List())))
 
 
   }
@@ -99,8 +100,10 @@ class FestivalDeInvierno extends FunSuite {
     var posta3 = Pesca(Some(10))
     var roco = FuriaNocturna(1, 3, 15, List(RequisitoBarbarosidad(1)))
     var roco2 = FuriaNocturna(1, 3, 15, List(RequisitoBarbarosidad(1)))
-    var torneo = Torneo(List(roco, roco2), List(posta, posta3))
-    assertResult(torneo.jugar(List(equipoVikingo,equipoVikingo1), ReglasPorEquipos()))(Some(List(Vikingo("Willy4",CaracteristicasVikingo(14.0,20.0,8.0,15.0),None,List()), Vikingo("Willy3",CaracteristicasVikingo(13.0,19.0,7.0,15.0),None,List()))))
+    var torneo = Torneo(List(roco, roco2), List(posta,posta3))
+    var estadoTorneo = RondaPorEquipos(List(equipoVikingo,equipoVikingo1))
+    var equipoGanador = EquipoVikingo(List(Vikingo("Willy4",CaracteristicasVikingo(14.0,20.0,8.0,15.0),None,List()), Vikingo("Willy3",CaracteristicasVikingo(13.0,19.0,7.0,15.0),None,List())))
+    assertResult(torneo.jugar(estadoTorneo)(ReglasPorEquipos()))(Some(equipoGanador))
 
   }
 
@@ -117,7 +120,9 @@ class FestivalDeInvierno extends FunSuite {
     var roco = FuriaNocturna(1, 3, 15, List(RequisitoBarbarosidad(1)))
     var roco2 = FuriaNocturna(1, 3, 15, List(RequisitoBarbarosidad(1)))
     var torneo = Torneo(List(roco, roco2), List(posta, posta3))
-    assertResult(torneo.jugar(List(equipoVikingo,equipoVikingo1), ReglasPorEquipos()))(Some(List(Vikingo("Willy4",CaracteristicasVikingo(14.0,20.0,8.0,20.0),None,List()))))
+    var estadoTorneo = RondaPorEquipos(List(equipoVikingo,equipoVikingo1))
+    var equipoGanador = EquipoVikingo(List(Vikingo("Willy4",CaracteristicasVikingo(14.0,20.0,8.0,20.0),None,List())))
+    assertResult(torneo.jugar(estadoTorneo)(ReglasPorEquipos()))(Some(equipoGanador))
 
   }
 
@@ -149,10 +154,9 @@ class FestivalDeInvierno extends FunSuite {
     var posta=Pesca(Some(10))
     var vikingo2 = Vikingo("Willy2",CaracteristicasVikingo(15,2,9,0),Some(SistemaDeVuelo), List())
     var torneo=Torneo(List(),List(posta))
-
-    assertResult(torneo.jugar(List(patapez,vikingo2), ReglasDeEliminacion(1)))(Some(patapez.modificarNivelHambre(0)))
+    var estadoTorneo = RondaEnCurso(List(patapez,vikingo2))
+    assertResult(torneo.jugar(estadoTorneo)(ReglasDeEliminacion(1)))(Some(patapez.modificarNivelHambre(0)))
 
   }
-
 
 }
